598d5581f90e6c11b7f79116f29a2b77a29bfa1b
ZSGH123 <2671242147@qq.com>
2019-06-23 13:07:26 +0800

    '创建用户模型'

---
+++ mskz-model/src/main/java/com/mskz/model/user/user.java
@@ -0,0 +1,11 @@
+package com.mskz.model.user;
+
+/**
+ * title: user
+ * projectName mskz
+ * description: mskz的用户信息model
+ * author biqi@raycloud.com
+ * date 2019-06-23 13:05
+ ***/
+public class user {
+}
